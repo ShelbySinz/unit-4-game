250d890524aa03bd953bb50172b26bcffadf7fc7
Shelby2 <shelbsjb@hotmail.com>
2018-12-07 15:17:06 -0700

    updated game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,22 +16,31 @@ var losses = 0;
 $("#wins").text(wins);
 $("#losses").text(losses);
 
-var isGameover = false;
+
 
 function reset(){
 targetNumber = Math.floor(Math.random()*101+19);
+
 counter = 0;
+
+$("#numberat, #number-to-guess").empty();
+
 }
 
 
-$("#Sock").on("click",function(){
+$(".one").on("click",function(){
+
+    alert("get those socks!");
 
 
-    var sockvalue = ($(this).val());
+    var sockvalue = $(this).val();
 
    sockvalue = parseInt(sockvalue);
 
-   counter += sockvalue;
+counter += sockvalue;
+
+   $("#numberat").text(counter);
+
 
   console.log(sockvalue);
 
@@ -39,14 +48,20 @@ $("#Sock").on("click",function(){
 
     if (counter === targetNumber){
         wins++;
+        $("#wins").text(wins);
         alert("you Win!");
         reset();
+        $("#number-to-guess").text(targetNumber);
+        $("#numberat").text(counter);
     }
 
     else if (counter >= targetNumber){
         losses++;
+        $("#losses").text(losses);
         alert("You Lose!");
         reset();
+        $("#number-to-guess").text(targetNumber);
+        $("#numberat").text(counter);
     }
 
 });
